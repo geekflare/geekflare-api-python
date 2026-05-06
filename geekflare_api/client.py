6d681869c6892e7e97f7830dfeaee2fc7610d535
Vaibhav Mishra <vaibhav@geekflare.com>
2026-05-06 11:50:16 +0530

    Refactor API method calls in GeekflareClient to use a unified _call method for improved readability and maintainability

--- geekflare_api/client.py
+++ geekflare_api/client.py
@@ -1,3 +1,4 @@
+import json
 import geekflare_api
 from geekflare_api.api.api_tool_api import ApiToolApi
 from geekflare_api.models import (
@@ -28,56 +29,60 @@ class GeekflareClient:
         self._client = geekflare_api.ApiClient(configuration)
         self._api = ApiToolApi(self._client)
 
+    def _call(self, method, **kwargs):
+        response = method(**kwargs)
+        return json.loads(response.data)
+
     def meta_scrape(self, body: MetaScrapeDto):
-        return self._api.meta_scrape(meta_scrape_dto=body)
+        return self._call(self._api.meta_scrape_without_preload_content, meta_scrape_dto=body)
 
     def web_scrape(self, body: WebScrapeDto):
-        return self._api.web_scrape(web_scrape_dto=body)
+        return self._call(self._api.web_scrape_without_preload_content, web_scrape_dto=body)
 
     def dns_record(self, body: DnsRecordDto):
-        return self._api.dns_record(dns_record_dto=body)
+        return self._call(self._api.dns_record_without_preload_content, dns_record_dto=body)
 
     def screenshot(self, body: ScreenshotDto):
-        return self._api.screenshot(screenshot_dto=body)
+        return self._call(self._api.screenshot_without_preload_content, screenshot_dto=body)
 
     def site_status(self, body: SiteStatusDto):
-        return self._api.site_status(site_status_dto=body)
+        return self._call(self._api.site_status_without_preload_content, site_status_dto=body)
 
     def redirect_check(self, body: RedirectCheckDto):
-        return self._api.redirect_check(redirect_check_dto=body)
+        return self._call(self._api.redirect_check_without_preload_content, redirect_check_dto=body)
 
     def broken_link(self, body: BrokenLinkDto):
-        return self._api.broken_link(broken_link_dto=body)
+        return self._call(self._api.broken_link_without_preload_content, broken_link_dto=body)
 
     def url2_pdf(self, body: Url2PdfDto):
-        return self._api.url2_pdf(url2_pdf_dto=body)
+        return self._call(self._api.url2_pdf_without_preload_content, url2_pdf_dto=body)
 
     def open_ports(self, body: OpenPortDto):
-        return self._api.open_ports(open_port_dto=body)
+        return self._call(self._api.open_ports_without_preload_content, open_port_dto=body)
 
     def tls_scan(self, body: TlsScanDto):
-        return self._api.tls_scan(tls_scan_dto=body)
+        return self._call(self._api.tls_scan_without_preload_content, tls_scan_dto=body)
 
     def load_time(self, body: LoadTimeDto):
-        return self._api.load_time(load_time_dto=body)
+        return self._call(self._api.load_time_without_preload_content, load_time_dto=body)
 
     def mixed_content(self, body: MixedContentDto):
-        return self._api.mixed_content(mixed_content_dto=body)
+        return self._call(self._api.mixed_content_without_preload_content, mixed_content_dto=body)
 
     def dns_sec(self, body: DnsSecDto):
-        return self._api.dns_sec(dns_sec_dto=body)
+        return self._call(self._api.dns_sec_without_preload_content, dns_sec_dto=body)
 
     def mtr(self, body: MtrDto):
-        return self._api.mtr(mtr_dto=body)
+        return self._call(self._api.mtr_without_preload_content, mtr_dto=body)
 
     def ping(self, body: PingDto):
-        return self._api.ping(ping_dto=body)
+        return self._call(self._api.ping_without_preload_content, ping_dto=body)
 
     def lighthouse(self, body: LighthouseDto):
-        return self._api.lighthouse(lighthouse_dto=body)
+        return self._call(self._api.lighthouse_without_preload_content, lighthouse_dto=body)
 
     def search(self, body: SearchRequestDto):
-        return self._api.search(search_request_dto=body)
+        return self._call(self._api.search_without_preload_content, search_request_dto=body)
 
     def __enter__(self):
         return self
